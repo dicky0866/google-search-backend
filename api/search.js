d5b1634bb030ae1c600689b50c6098768669b189
dicky0866 <dickyoktaftafonao@gmail.com>
2026-01-28 19:20:11 +0700

    Update search.js

--- api/search.js
+++ api/search.js
@@ -5,18 +5,16 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: "Parameter q wajib ada" });
   }
 
-  const apiKey = process.env.GOOGLE_API_KEY;
-  const cx = process.env.GOOGLE_CX;
-
-  const url = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(
-    q
-  )}&key=${apiKey}&cx=${cx}`;
-
   try {
+    const url = `https://api.duckduckgo.com/?q=${encodeURIComponent(
+      q
+    )}&format=json&no_redirect=1&no_html=1`;
+
     const response = await fetch(url);
     const data = await response.json();
+
     res.status(200).json(data);
   } catch (err) {
-    res.status(500).json({ error: "Gagal fetch Google Search" });
+    res.status(500).json({ error: "Gagal fetch DuckDuckGo" });
   }
 }
